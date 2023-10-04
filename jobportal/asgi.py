3b374f2d929246bc5157bbe36d3f026c70fe587c
Aysha N <you@example.com>
2023-10-04 11:55:09 +0530

    [UPDT]

--- jobportal/asgi.py
+++ jobportal/asgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobportal.settings')
-print("HHii")
+print('ffff00000")
 
 application = get_asgi_application()
